5814c17a89da6bebcdba7c6e4240af9d1e212b86
Jaco Ruit <jacoruit@live.nl>
2013-01-27 00:58:09 +0100

    works

--- image.js
+++ image.js
@@ -1,12 +1,8 @@
-steganos.Image = function(image){
-	this.canvas = document.createElement("canvas");
-	this.context = this.canvas.getContext("2d");
-	this.canvas.width = image.width;
-	this.canvas.height = image.height;
-	this.context.drawImage(image, 0, 0);
-	this.bytes = this.context.getImageData(0, 0, image.width, image.height);
-	this.original = this.bytes;
+steganos.Image = function(bytes){
+	this.bytes = bytes;
 	this.chunks = {};
+	this.offset = Math.floor(this.bytes.length / 8) * 8 / 2;
+	this.capicity = this.offset / 8; 
 };
 
 steganos.Image.prototype.getHeaderBytes = function(){
@@ -31,37 +27,33 @@ steganos.Image.prototype.deallocChunck = function(name){
 
 steganos.Image.prototype.write = function(){
 	var header = this.getHeaderBytes();
-	for(var i = 0; i < this.original.data.length; ++i){
-		this.bytes.data[i] = this.original.data[i];
-	}
-	var pos = this.writeBytes(0, header);
-	//console.log(pos, header);
+	var saved = 0;
+	var pos = this.offset;
+	pos = this.writeBytes(pos, header);
+	saved += header.length;
 	for(var chunk in this.chunks){
-		//console.info(pos);
+		if(saved + 2 + this.chunks[chunk].length > this.capicity){
+			throw "Too much data, can't save chunk: " + chunk;
+		}
 		pos = this.writeByte(pos, steganos.CHUNK_START);
 		pos = this.writeBytes(pos, this.chunks[chunk]);
 		pos = this.writeByte(pos, steganos.CHUNK_END);
-		//console.log(pos);
 	}
-	this.flush();
 };
 
 steganos.Image.prototype.writeByte = function(pos, byte){
 	if(byte > 255) throw "This isn't a byte";
-	//console.info("Writing ", byte, "(", byte.toString(2) ,") at ", pos);
 	for(var i = 0; i < 8; ++i){	
 		if(byte & 1 == 1)
-			this.bytes.data[pos + i] |= 1; 
+			this.bytes[pos + i] |= 1; 
 		else	
-			this.bytes.data[pos + i] &= ~1; 
-		//console.log(byte.toString(2), this.bytes.data[pos + i].toString(2));
+			this.bytes[pos + i] &= ~1; 
 		byte >>= 1;
 	}
 	return pos + 8;
 };
 
 steganos.Image.prototype.writeBytes = function(pos, bytes){
-	//console.log(pos);
 	for(var i = 0; i < bytes.length; ++i){
 		pos = this.writeByte(pos, bytes[i]);
 	}
@@ -69,16 +61,18 @@ steganos.Image.prototype.writeBytes = function(pos, bytes){
 };
 
 steganos.Image.prototype.read = function(){
-	var bytes = this.readBytes(0, (this.canvas.width * this.canvas.height - 8) / 8);
+	var bytes = this.readBytes(this.offset, this.capicity);
 	var bytePos = null;	for(var i = 0; i < bytes.length; ++i){
-		if(bytes[i] == steganos.HEADER_START) bytePos = i + 1;
+		if(bytes[i] == steganos.HEADER_START){
+			bytePos = i + 1;
+			break;
+		}
 	}
 	if(bytePos == null) throw "Invalid steganos image";
 	var byte = bytes[bytePos];
 	var inChunk = false;
 	var chunkNameBytes = [];
 	this.chunks = {};
-	var chunkOrder = [];
 	while(byte != steganos.HEADER_END && byte != undefined){	
 		if(byte == steganos.CHUNK_START && !inChunk){
 			chunkNameBytes = [];
@@ -86,7 +80,6 @@ steganos.Image.prototype.read = function(){
 		}else if(byte == steganos.CHUNK_END && inChunk){ 
 			var chunkName = steganos.bytesToString(chunkNameBytes);
 			this.chunks[chunkName] = null;
-			chunkOrder.push(chunkName);
 			inChunk = false;
 		}else if(inChunk)
 			chunkNameBytes.push(byte);
@@ -102,37 +95,25 @@ steganos.Image.prototype.read = function(){
 		}
 		this.chunks[chunk] = chunkBytes;
 	};
-	//console.log(this.chunks);
 };	
 
 steganos.Image.prototype.readByte = function(pos){
-	//console.info("Reading byte at ", pos);
 	var byte = 0;
 	for(var i = 0; i < 8; ++i){
-		if(this.bytes.data[pos + i] & 1 == 1)
+		if(this.bytes[pos + i] & 1 == 1)
 			byte |= Math.pow(2, i);
-		//console.log(this.bytes.data[pos + i].toString(2), (this.bytes.data[pos + i] & 1), byte.toString(2));
 	}
 	return byte;
 };
 
-steganos.Image.prototype.readBytes = function(pos, count){
+steganos.Image.prototype.readBytes = function(pos, max){
 	var bytes = [];
-	for(var i = 0; i < count; ++i){
+	for(var i = 0; i < max; ++i){
 		bytes.push(this.readByte(pos + i * 8));
 	}
 	return bytes;
 };
 
-
-steganos.Image.prototype.flush = function(){
-	this.canvas.width = this.canvas.width;
-	this.context.putImageData(this.bytes, 0, 0);
-	this.bytes = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
-};
-
 steganos.Image.prototype.getDataUri = function(format){
-	//return "data:image/png;base64," + steganos.bytesToBase64(this.bytes.data);
-	this.flush();
-	return this.canvas.toDataURL(format);
+	return steganos.DATA_URI_JPG_START + steganos.bytesToBase64(this.bytes);
 };
--- steganos.js
+++ steganos.js
@@ -1,12 +1,14 @@
 var steganos = {};
 
-steganos.HEADER_START = 4; 	// 	00000100
-steganos.HEADER_END = 6; 		// 	00000110
-steganos.CHUNK_START = 8;		// 	00001000
-steganos.CHUNK_END = 10;		// 	00001010
-steganos.CHUNK_SIZE = 12;		//  00001100
+steganos.HEADER_START = 4; 	
+steganos.HEADER_END = 6; 		
+steganos.CHUNK_START = 8;		
+steganos.CHUNK_END = 10;		
+steganos.CHUNK_SIZE = 12;		
 
-steganos.bytesToString = function(byteArray){
+steganos.DATA_URI_JPG_START = "data:image/jpeg;base64,";
+
+steganos.bytesToString = function(byteArray, xhrEscape){
 	var string = "";
 	for(var i = 0; i < byteArray.length; ++i){
 		string += String.fromCharCode(byteArray[i]);
@@ -14,14 +16,30 @@ steganos.bytesToString = function(byteArray){
 	return string;
 };
 
-steganos.stringToBytes = function(string){
+steganos.stringToBytes = function(string, xhrEscape){
 	var bytes = [];
 	for(var i = 0; i < string.length; ++i){
-		bytes.push(string.charCodeAt(i));
+		if(xhrEscape)
+			bytes.push(string.charCodeAt(i) & 0xff);
+		else
+			bytes.push(string.charCodeAt(i));
 	}
 	return bytes;
 };
 
 steganos.bytesToBase64 = function(byteArray){
 	return window.btoa(steganos.bytesToString(byteArray));
+};
+
+steganos.base64ToBytes = function(string){
+	return steganos.stringToBytes(window.atob(string));
+};
+
+steganos.getImageBytes = function(imageUrl){
+	var request = new XMLHttpRequest();
+	request.open("GET", imageUrl, false);
+	request.overrideMimeType('text\/plain; charset=x-user-defined');
+	request.send(null);
+	if (request.status != 200) return 0;
+	return steganos.stringToBytes(request.responseText, true);
 };
